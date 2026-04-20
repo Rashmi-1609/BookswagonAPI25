483def1d0ddd4b5859680d2813d1b60e6273d07e
krishna-bookswagon <krishna.bansal@bookswagon.in>
2026-04-20 16:06:35 +0530

    refactor(PublisherService.Api/Infrastructure/Data): implement primary constructor in AppDbContext

--- PublisherService.Api/Infrastructure/Data/AppDbContext.cs
+++ PublisherService.Api/Infrastructure/Data/AppDbContext.cs
@@ -3,14 +3,9 @@ using PublisherService.Api.Domain.Entities;
 
 namespace PublisherService.Api.Infrastructure.Data;
 
-// AppDbContext inherits from DbContext (the core EF class).
-public class AppDbContext : DbContext
+// using primary constructor syntax to inject the DbContextOptions dependency
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
-    // Constructor accepts options (like the connection string) passed from Program.cs via Dependency Injection.
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-    {
-    }
-
     // DbSet represents the collection of all Publisher entities in the database.
     // This is what our Repository will actually query against.
     public DbSet<Publisher> Publishers { get; set; }
