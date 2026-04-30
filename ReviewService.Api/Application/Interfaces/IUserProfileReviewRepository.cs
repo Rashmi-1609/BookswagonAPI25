e83bc2b3f75d008bfec5679b15fd1ac1cc438a2f
krishna-bookswagon <krishna.bansal@bookswagon.in>
2026-04-30 17:27:41 +0530

    feat(review-service): add user profile repository with sql mapping

---
+++ ReviewService.Api/Application/Interfaces/IUserProfileReviewRepository.cs
@@ -0,0 +1,5 @@
+﻿namespace ReviewService.Api.Application.Interfaces;
+
+public interface IUserProfileReviewRepository
+{
+}
---
+++ ReviewService.Api/Infrastructure/Repositories/UserProfileReviewRepository.cs
@@ -0,0 +1,5 @@
+﻿namespace ReviewService.Api.Infrastructure.Repositories;
+
+public class UserProfileReviewRepository
+{
+}
